f2af37d97125ec5afeaadef42c0b1a62e44ff7f5
Jaekwang Seo <crzjaek@vt.edu>
2013-05-09 13:48:51 -0400

    user authorization implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
   
   def show
   	@user = User.find(params[:id])
@@ -13,10 +15,41 @@ class UsersController < ApplicationController
   	if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
-  		redirect_to @user
+      redirect_to @user
   	else
   		render 'new'
   	end
   end
 
+  def edit
+    # Unnecessary because of before_filter :correct_user
+    # @user = User.find(params[:id])
+  end
+
+  def update
+    # Unnecessary because of before_filter :correct_user
+    # @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      #current_user? is defined in sessions helper
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
